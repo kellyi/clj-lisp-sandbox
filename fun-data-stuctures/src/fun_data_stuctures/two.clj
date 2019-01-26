8a589d2e003a5a99159cc0b14a9e0f70b64f2d1a
Kelly Innes <kinnes@gmail.com>
2019-01-26 12:52:59 -0500

    Add function to create a complete tree

--- fun-data-stuctures/src/fun_data_stuctures/two.clj
+++ fun-data-stuctures/src/fun_data_stuctures/two.clj
@@ -66,14 +66,14 @@
 
 (def single-node (create-bs-tree-node 20 nil nil))
 (bs-tree-member? 0 single-node)
-(bs-tree-member? 1 single-node)
-(bs-tree-member? 2 single-node)
+(bs-tree-member? 20 single-node)
 
 (defn bs-tree-insert
   "Create a new node to store the given value in a binary search tree"
   [value node]
   {:pre [(spec/valid? ::value value)
-         (spec/valid? ::bs-tree-node? node)]}
+         (spec/valid? ::bs-tree-node? node)]
+   :post [(spec/valid? ::bs-tree-node? %)]}
   (cond
     (nil? node) (create-bs-tree-node value nil nil)
     (< value (:value node)) (create-bs-tree-node (:value node)
@@ -84,11 +84,73 @@
                                                  (bs-tree-insert value (:right node)))
     :else node))
 
-(as-> single-node tree
-  (bs-tree-insert -2 tree)
-  (bs-tree-insert 10 tree)
-  (bs-tree-insert 5 tree)
-  (bs-tree-insert 1000 tree)
-  (bs-tree-insert 50 tree)
-  (bs-tree-insert -50 tree)
-  (bs-tree-insert 10 tree))
+(def multi-node (as-> single-node tree
+                  (bs-tree-insert -2 tree)
+                  (bs-tree-insert 10 tree)
+                  (bs-tree-insert 5 tree)
+                  (bs-tree-insert 1000 tree)
+                  (bs-tree-insert 50 tree)
+                  (bs-tree-insert -50 tree)
+                  (bs-tree-insert 10 tree)))
+
+;; 2.2 adjust tree-member? function to defer equality check until the end
+(defn has-right-node?
+  "Check whether a binary search tree node has a right node"
+  [node]
+  {:pre [(spec/valid? ::bs-tree-node? node)]}
+  (cond
+    (nil? (:right node)) false
+    :else true))
+
+(defn has-left-node?
+  "Check whether a binary search tree node has a left node"
+  [node]
+  {:pre [(spec/valid? ::bs-tree-node? node)]}
+  (cond
+    (nil? (:left node)) false
+    :else true))
+
+(defn is-leaf-node?
+  "Check whether a binary search tree node is a leaf node"
+  [node]
+  {:pre [(spec/valid? ::bs-tree-node? node)]}
+  (and (-> node has-left-node? not)
+       (-> node has-right-node? not)))
+
+;; (defn defer-bs-tree-member?
+;;   "check whether a value is a member of the set stored in a binary search tree"
+;;   [value node]
+;;   {:pre [(spec/valid? ::value value)
+;;          (spec/valid? ::bs-tree-node? node)]}
+;;   (cond
+;;     (< value (:value node)) (recur value (:left node))
+;;     :else (recur value (:right node))))
+
+(is-leaf-node? single-node)
+(is-leaf-node? multi-node)
+
+;; 2.3 Rewrite insert using execptions to avoid copying
+
+;; ...
+
+;; 2.4 Combine defer-bs-tree-member? and exception-insert? into a more efficient
+;; insert function
+
+;; ...
+
+;; 2.5 Write a function `complete` where `(complete x d)` creates a complete tree of
+;; depth d with x stored in every node
+
+(defn create-complete-tree
+  "Create a complete tree of depth d with value x stored at each node"
+  [x d]
+  {:pre [(spec/valid? int? x)
+         (spec/valid? int? x)]
+   :post [(spec/valid? ::bs-tree-node? %)]}
+  (cond
+   (zero? d) (create-bs-tree-node x nil nil)
+   :else (create-bs-tree-node x
+                              (create-complete-tree x (dec d))
+                              (create-complete-tree x (dec d)))))
+
+(create-complete-tree 10 5)
