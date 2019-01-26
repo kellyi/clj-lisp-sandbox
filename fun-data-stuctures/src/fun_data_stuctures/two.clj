1377132875b86d69684ec19e899ede3141998c42
Kelly Innes <kinnes@gmail.com>
2019-01-25 21:47:34 -0500

    Create base bs tree fn + spec

--- fun-data-stuctures/src/fun_data_stuctures/two.clj
+++ fun-data-stuctures/src/fun_data_stuctures/two.clj
@@ -1,5 +1,22 @@
-(ns fun-data-stuctures.two)
+(ns fun-data-stuctures.two
+  (:require [clojure.spec.alpha :as spec]))
 
+(defn ++
+  [[x & xs] ys]
+  (cond
+    (nil? x) ys
+    :else (cons x (++ xs ys))))
+
+(++ '(1 2 3) '(4 5 6))
+
+(defn fun-update
+  [[x & xs] i y]
+  (cond
+    (nil? x) nil
+    (zero? i) (cons y xs)
+    :else (cons x (fun-update xs (- i 1) y))))
+
+(fun-update '(1 2 3 4 5 6 7 8 9 10) 4 100)
 
 ;; Exercise 2.1
 ;;
@@ -14,3 +31,17 @@
      :else (suffixes (rest xs) (conj accumulator xs)))))
 
 (suffixes '(1 2 3 4))
+
+(spec/def ::bs-tree-node
+  (spec/keys :req-un [::value int?
+                      ::right (or nil? ::bs-tree-node)
+                      ::left (or nil? ::bs-tree-node)]))
+
+(defn create-bs-tree-node
+  [value left right]
+  {:post [(spec/valid? ::bs-tree-node %)]}
+  {:value value
+   :left left
+   :right right})
+
+(create-bs-tree-node 1 (create-bs-tree-node 2 nil nil) nil)
